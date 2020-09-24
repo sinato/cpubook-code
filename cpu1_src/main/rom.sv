eef949af6ed5dd8a98a8bfa07834fe1a5c16db17
Shingo Kato <katomail0510@gmail.com>
2020-09-24 21:05:24 +0900

    Add rom module

---
+++ cpu1_src/main/rom.sv
@@ -0,0 +1,13 @@
+module rom(
+  input  logic addr,
+  output logic data
+);
+
+always_comb begin
+  case (addr)
+    1'b0: data = 1'b1; // NOT
+    1'b1: data = 1'b0; // NOP
+  endcase
+end
+
+endmodule
